09e861b2a09b52e3e7f5303b4cac5986bf2999c1
realhardworkingdeveloper <realhardworkingdeveloper@gmail.com>
2022-03-02 22:54:25 +0900

    updated frontend

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import *
   
 app = Flask(__name__) #creating the Flask class object   
  
-@app.route('/') #decorator drfines the   
+@app.route('/')
 def home():  
-    return render_template('home.html');  
+    return render_template('home.html')
+
+@app.route('/report')
+def report():  
+    return render_template('report.html')
   
 if __name__ =='__main__':  
     app.run(debug = True)  
