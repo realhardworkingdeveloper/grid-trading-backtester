76f3296cf054de5e2deaf46214fb3b05b889fd45
realhardworkingdeveloper <realhardworkingdeveloper@gmail.com>
2022-03-03 00:17:50 +0900

    update frontend

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -9,38 +9,39 @@ Author: GrayGrids
     window.onload = function () {
         window.setTimeout(fadeout, 500);
 
-        var from_$input = $('#input_from').pickadate(),
-          from_picker = from_$input.pickadate('picker')
-      
-        var to_$input = $('#input_to').pickadate(),
-          to_picker = to_$input.pickadate('picker')
-      
-      
-        // Check if there’s a “from” or “to” date to start with.
-        if ( from_picker.get('value') ) {
-          to_picker.set('min', from_picker.get('select'))
-        }
-        if ( to_picker.get('value') ) {
-          from_picker.set('max', to_picker.get('select'))
-        }
-      
-        // When something is selected, update the “from” and “to” limits.
-        from_picker.on('set', function(event) {
-          if ( event.select ) {
-            to_picker.set('min', from_picker.get('select'))    
+        var nowTemp = new Date();
+        var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+
+        var startDate = $('#StartDate').datepicker({
+
+          beforeShowDay: function(date) {
+            return date.valueOf() >= now.valueOf();
+          },
+          autoclose: true
+
+        }).on('changeDate', function(ev) {
+          if (ev.date.valueOf() > endDate.datepicker("getDate").valueOf() || !endDate.datepicker("getDate").valueOf()) {
+
+            var newDate = new Date(ev.date);
+            newDate.setDate(newDate.getDate() + 1);
+            endDate.datepicker("update", newDate);
+
           }
-          else if ( 'clear' in event ) {
-            to_picker.set('min', false)
-          }
-        })
-        to_picker.on('set', function(event) {
-          if ( event.select ) {
-            from_picker.set('max', to_picker.get('select'))
-          }
-          else if ( 'clear' in event ) {
-            from_picker.set('max', false)
-          }
-        })
+          $('#EndDate')[0].focus();
+        });
+
+
+        var endDate = $('#EndDate').datepicker({
+          beforeShowDay: function(date) {
+            if (!startDate.datepicker("getDate").valueOf()) {
+              return date.valueOf() >= new Date().valueOf();
+            } else {
+              return date.valueOf() > startDate.datepicker("getDate").valueOf();
+            }
+          },
+          autoclose: true
+
+        }).on('changeDate', function(ev) {});
     }
 
     function fadeout() {
